ac61fb6298a3b0556c8651d69a99d6acd6e7eeab
Gary O Brien <t00181299@365s.ittralee.ie>
2016-10-17 14:04:45 +0100

    Enumerations
    Changed values to enumeration values

---
+++ Builder.java
@@ -0,0 +1,6 @@
+/**
+ * Created by t00181299 on 26/09/2016.
+ */
+public enum Builder {
+    FENDER, MARTIN, GIBSON, COLLINGS, OLSIN, RYAN, PRS, ANY;
+}
---
+++ Type.java
@@ -0,0 +1,6 @@
+/**
+ * Created by t00181299 on 26/09/2016.
+ */
+public enum Type {
+    ACOUSTIC, ELECTRIC;
+}
